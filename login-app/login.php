1161381f0f202c9dfa5225ccd6e727d27c527407
syahdan martua <syahdanmartua11@gmail.com>
2025-07-20 19:17:26 +0700

    login form, kurang ke index utama

---
+++ login-app/login.php
@@ -0,0 +1,20 @@
+<?php
+include 'koneksi.php';
+session_start();
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+$result = mysqli_query($koneksi, $query);
+
+if (mysqli_num_rows($result) > 0) {
+    $_SESSION['username'] = $username;
+    header("Location: dashboard.php");
+} else {
+    echo "<script>
+        alert('Login gagal! Username atau password salah.');
+        window.location.href = 'index.html';
+    </script>";
+}
+?>
